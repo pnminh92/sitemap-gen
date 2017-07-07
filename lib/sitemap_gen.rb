0f1ce78a6f3932b1bd758f41bb89b5558ce634e5
Minh Phan <wofi.minh@1pac.vn>
2017-07-07 14:14:57 +0700

    Fix csv generator

--- lib/sitemap_gen.rb
+++ lib/sitemap_gen.rb
@@ -1,64 +1,78 @@
 require 'nokogiri'
 require 'csv'
+require 'benchmark'
+require 'sitemap_gen/csv'
 
 module SitemapGen
-  IGNORE_DIRS_REGEX = /img|cgi-bin|images|css|js/i
-
-  class << self
-    def run(dir_path, base_url, save_path = nil)
-      generate_csv(csv_data(dir_path, base_url), save_path)
-    end
-
-    def generate_csv(data, save_path)
-      level_header = data.each_with_object([]) { |item, o| o << item[:levels] }
-                         .inject([]) { |max, row| max.size < row.keys.size ? row.keys : max }
-      save_path ||= Dir.pwd
-      CSV.open("#{save_path}/sitemap.csv", 'wb') do |csv|
-        csv << ['id'].concat(level_header).push('url')
-        data.each_with_index do |row, i|
-          gap = level_header.length - row[:levels].values.length
-          csv << [i + 1].concat(row[:levels].values).concat(Array.new(gap) { '' }).push(row[:url])
-        end
-      end
-    end
-
-    def csv_data(dir_path, base_url)
-      # If there is a foward slash at the end of dir path then remove it
-      #dir_path = dir_path[0..-2] if dir_path[-1] =~ /\//
-
-      # Exit if there is no html files
-      html_files = Dir.glob("#{dir_path}/**/*.html")
-      exit if html_files.empty?
-
-      data = []
-      html_files.each_with_index do |file_path, i|
-        next if file_path =~ IGNORE_DIRS_REGEX
-        server_pathname = file_path.sub(dir_path, '')
-        base_path = File.dirname(server_pathname)
-        last_slash = base_path == '/' ? '' : '/'
-        data.push({ url: base_url + base_path + last_slash}
-                  .merge({ levels: dir_levels(dir_path, server_pathname) }))
-      end
-      data
-    end
-
-    def page_title(file_path)
-      html_doc = Nokogiri::HTML(File.read(file_path))
-      html_doc.css('head title').first.content
-    end
-
-    def dir_levels(dir_path, server_pathname)
-      levels = {}
-      dirs = server_pathname.split('/')
-
-      # Drop first and last element of dirs array, because they are a empty string and a filename
-      dirs[1..-2].each_with_index do |dir, i|
-        current_dir_index = dirs.index(dir)
-        current_path = dirs[0..current_dir_index].join('/')
-        html_file = Dir.glob("#{dir_path}#{current_path}/index.html").first
-        levels.merge!({"level_#{i + 1}": page_title(html_file)})
-      end
-      levels
-    end
+  def self.run(dir_path, base_url, save_path = nil)
+    SitemapGen::Csv.new(dir_path, base_url, save_path).generate
   end
+  #class << self
+    #def run(dir_path, base_url, save_path = nil)
+      #p Benchmark.measure { generate_csv(csv_data(dir_path, base_url), save_path) }
+    #end
+
+    #def generate_csv(data, save_path)
+      #lheaders = level_headers(data)
+      #save_path ||= Dir.pwd
+      #CSV.open("#{save_path}/sitemap.csv", 'wb') do |csv|
+        #csv << ['id'].concat(lheaders).push('url')
+        #data.each_with_index do |row, i|
+          #gap = lheaders.length - row[:levels].values.length
+          #csv << [i + 1].concat(row[:levels].values).concat(Array.new(gap) { '' }).push(row[:url])
+        #end
+      #end
+    #end
+
+    #def level_headers(data)
+      #data.each_with_object([]) { |item, o| o << item[:levels] }
+          #.inject([]) { |max, row| max.size < row.keys.size ? row.keys : max }
+    #end
+
+    #def csv_data(dir_path, base_url)
+      ## Raise error if there is no html files
+      #html_files = Dir.glob("#{dir_path}/**/index.html")
+      #raise 'There is no html files in your directory' if html_files.empty?
+
+      #data = []
+      #html_files.each_with_index do |file_path, i|
+        #next if file_path =~ IGNORE_DIRS_REGEX
+        #page_path = file_path.sub(dir_path, '')
+        #base_path = File.dirname(page_path)
+        #last_slash = base_path == '/' ? '' : '/'
+        #short_page_path = page_path.split('/')[0..-2].join('/')
+        #data.push({ url: base_url + base_path + last_slash, page_path: short_page_path == '' ? '/' : short_page_path  }
+                  #.merge({ levels: dir_levels(dir_path, page_path) }))
+      #end
+      #organized_data(dir_path, data)
+    #end
+
+    #def page_title(file_path)
+      #html_doc = Nokogiri::HTML(File.read(file_path))
+      #html_doc.css('head title').first.content
+    #end
+
+    #def dir_levels(dir_path, page_path)
+      #levels = {}
+      #order = 0
+      #page_path.split('/')[1..-2].each_with_index do |dir, i|
+        #order = i + 1
+        #levels.merge!({"level_#{order}": ''})
+      #end
+      #html_file = Dir.glob("#{dir_path}#{page_path}").first
+      #levels.merge!({"level_#{order + 1}": page_title(html_file)})
+      #levels
+    #end
+
+    #def organized_data(dir_path, data)
+      #top_level_dirs = Dir.glob("#{dir_path}/**/index.html").map { |path| File.dirname(path).sub(dir_path, '').split('/')[0..-1].join('/') }.uniq
+      #organized_data = []
+      #organized_data.concat data.select { |d| d[:page_path] == '/' }
+      #top_level_dirs.each do |dir|
+        ## We group data by page path and then order by alphabet
+        #organized_data.concat(data.select { |d| d[:page_path] =~ /\A#{Regexp.quote(dir)}\z/ }.sort_by { |d| d[:page_path] })
+      #end
+      #organized_data
+    #end
+  #end
 end
---
+++ lib/sitemap_gen/csv.rb
@@ -0,0 +1,74 @@
+module SitemapGen
+  class Csv
+    IGNORE_DIRS_REGEX = /img|cgi-bin|images|css|js/i
+
+    def initialize(dir_path, base_url, save_path)
+      @dir_path = dir_path
+      @base_url = base_url
+      @save_path = save_path || Dir.pwd
+      @max_level = 1
+      @html_files = Dir.glob("#{dir_path}/**/index.html").sort_by { |f| File.dirname(f) }
+      raise 'There is no index.html files in your directory' if @html_files.empty?
+      @sitemaps = create_sitemaps
+    end
+
+    def generate
+      CSV.open("#{@save_path}/sitemap.csv", 'wb') do |csv|
+        csv << csv_header
+        @sitemaps.each_with_index { |item, i| csv << csv_row(item, i) }
+      end
+    end
+
+    private
+
+      def create_sitemaps
+        sitemaps = []
+        @html_files.each do |f|
+          next if f =~ IGNORE_DIRS_REGEX
+          sitemaps.push({ url: @base_url + server_path(f), levels: dir_levels(f) })
+        end
+        sitemaps
+      end
+
+      def csv_header
+        header = ['Id']
+        @max_level.to_i.times.each { |l| header.push("Level #{l + 1}") }
+        header.push('Url')
+      end
+
+      def csv_row(item, order_num)
+        gap = @max_level - item[:levels].values.size
+        [order_num + 1].concat(item[:levels].values).concat(Array.new(gap) { '' }).push(item[:url])
+      end
+
+      def html_page_title(file_path)
+        html_doc = Nokogiri::HTML(File.read(file_path))
+        html_doc.css('head title').first.content
+      end
+
+      def dir_levels(file_path)
+        p file_path
+        levels = {}
+        order = 0
+        dirs = server_path(file_path).split('/')
+        if dirs.empty?
+          levels.merge!({"level_#{order += 1}": html_page_title(file_path)})
+        else
+          dirs[1..-1].each_with_index do |dir, i|
+            levels.merge!({"level_#{order += 1}": ''})
+          end
+          levels.merge!({"level_#{order += 1}": html_page_title(file_path)})
+        end
+        set_max_level(order)
+        levels
+      end
+
+      def server_path(file_path)
+        File.dirname(file_path.sub(@dir_path, ''))
+      end
+
+      def set_max_level(num)
+        @max_level = num > @max_level ? num : @max_level
+      end
+  end
+end
